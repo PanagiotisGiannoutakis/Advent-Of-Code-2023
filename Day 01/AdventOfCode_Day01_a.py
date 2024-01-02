588850c5c0c68ac4c7c967e72896647464a7d014
Panagiotis Giannoutakis <giannou1993@gmail.com>
2024-01-02 20:01:32 +0200

    Update AdventOfCode_Day01_a.py

--- Day 01/AdventOfCode_Day01_a.py
+++ Day 01/AdventOfCode_Day01_a.py
@@ -1,1 +1,14 @@
+import re
 
+file_input = open('AdventOfCode_Day01_Input.txt', 'r')
+lines = file_input.readlines()
+sum = 0
+for line in lines:
+    line_splitted = re.findall("[0-9]", line)
+    print(line_splitted)
+    line_number = line_splitted[0] + line_splitted[(len(line_splitted)-1)]
+    sum += int(line_number)
+    print(line_splitted[0])
+    print(line_splitted[(len(line_splitted)-1)])
+    print(int(line_number))
+print(sum)
